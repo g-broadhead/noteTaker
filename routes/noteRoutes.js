16bdc3e3732fa800c9600d459a276737b43feb3e
gbroadhead <gwbroadhead@gmail.com>
2021-12-14 21:28:08 -0800

    UUID implemented, delete function working

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,9 +1,10 @@
+// Required Routes
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
 
-
+// Get Notes function
 router.get('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) { console.log(err) }
@@ -11,26 +12,35 @@ router.get('/api/notes', (req, res) => {
   })
 })
 
+// Post Notes function
 router.post('/api/notes', (req, res) => {
-  const item = req.body
+  const note = req.body
   note.id = uuidv4();
-  fs.readFile(path.join(__dirname,'..', 'db', 'db.json'), 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) { console.log(err) }
-    const items = JSON.parse(data)
-    items.push(item)
-    fs.writeFile(path.join(__dirname,'..', 'db', 'db.json'), JSON.stringify(items), err => {
+    const notes = JSON.parse(data)
+    notes.push(note)
+    fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
       if (err) { console.log(err) }
       res.sendStatus(200)
     })
   })
 })
 
-router.delete('/remove', (req, res) => {
-  const items = req.body
-  console.log(req.body)
-  fs.writeFile(path.join(__dirname,'..', 'db', 'db.json'), JSON.stringify(items), err => {
-    if (err) { console.log(err) }
-    res.sendStatus(200)
+// Delete function
+router.delete('/api/notes/:id', (req, res) => {
+  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    const notes = JSON.parse(data)
+    for (let i = 0; i < notes.length; i++) {
+      const dbNotes = notes[i];
+      if (dbNotes.id === req.params.id) {
+        notes.splice(i, 1)
+      }
+    }
+    fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
+      if (err) { console.log(err) }
+      res.sendStatus(200)
+    })
   })
 })
 
